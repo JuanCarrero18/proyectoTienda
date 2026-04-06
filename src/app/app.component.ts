d7b620c0f1f53dfb5a20e94c99fa74ff63969931
jcarrerg <jcarrerg@emeal.nttdata.com>
2026-04-06 13:16:55 +0200

    eliminar codigo residual

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +9,4 @@ export class AppComponent {
   title = 'tienda';
 
   constructor() {}
-
-  ngOnInit(): void {
-
-
-  }
-
-
 }
